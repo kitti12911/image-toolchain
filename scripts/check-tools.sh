08eeebead621e3e9561f5d60c7fc93ec0250571a
fmt.Println("K") <50092335+kitti12911@users.noreply.github.com>
2026-05-11 20:54:56 +0700

    feat!: make application rely on custom image instead of github actions for more compatible when transition to gitlab ci (#17)

--- scripts/check-tools.sh
+++ scripts/check-tools.sh
@@ -13,14 +13,34 @@ case "${profile}" in
 		docker run --rm "${image}" protoc --version
 		docker run --rm "${image}" protoc-gen-go --version
 		docker run --rm "${image}" protoc-gen-go-grpc --version
+		docker run --rm "${image}" golangci-lint version
 		docker run --rm "${image}" fieldmapgen -h
 		docker run --rm "${image}" patchfieldgen -h
 		docker run --rm "${image}" goose -version
 		docker run --rm "${image}" sqlfluff --version
+		docker run --rm "${image}" helm version --short
+		docker run --rm "${image}" oasdiff --help
+		;;
+	release)
+		docker run --rm "${image}" git --version
+		docker run --rm "${image}" node --version
+		docker run --rm "${image}" npm --version
+		docker run --rm "${image}" markdownlint-cli2 --version
+		docker run --rm "${image}" release-please --version
+		docker run --rm "${image}" semantic-release --version
+		docker run --rm "${image}" npm list --global --depth=0 \
+			@semantic-release/commit-analyzer \
+			@semantic-release/github \
+			@semantic-release/gitlab \
+			@semantic-release/release-notes-generator \
+			conventional-changelog-conventionalcommits
 		;;
 	security)
 		docker run --rm "${image}" govulncheck -version
 		docker run --rm "${image}" semgrep --version
+		docker run --rm "${image}" trivy --version
+		docker run --rm "${image}" gitleaks version
+		docker run --rm "${image}" cosign version
 		;;
 	*)
 		echo "unknown profile: ${profile}" >&2
